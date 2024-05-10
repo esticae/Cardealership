17f35b35fc91e42e9cbf7789b65a6389addbc9b8
esticae <esticaeugene@gmail.com>
2024-05-10 14:24:09 -0400

    added constructors

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    public Dealership(String address, String phone, String name) {
+        this.address = address;
+        this.phone = phone;
+        this.name = name;
+    }
+}
